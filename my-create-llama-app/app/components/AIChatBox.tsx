26cca72ada46603cc4be5a88021e387f8bdf9e08
ninemean <167946235+ninemean@users.noreply.github.com>
2024-04-25 09:26:21 +0000

    fix: eslint

--- my-create-llama-app/app/components/AIChatBox.tsx
+++ my-create-llama-app/app/components/AIChatBox.tsx
@@ -72,32 +72,28 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
             />
           )}
           {!error && messages.length === 0 && (
-            <div className="mx-8 flex h-full flex-col items-center justify-center gap-3 text-center">
-              <Bot size={28} />
-              <p className="text-lg font-medium">
-                권구민 지원자에 대해 뭐든지 물어보세요!
-              </p>
-              <p>
-                그럼, AI 구민이 당신의 질문에 대해 모두 답변 할 것입니다!<br/>
-                만약 어떤 질문을 해야할 지 모르겠다면 <br/> "질문 추천해줘" 라고 물어보세요!
-              </p>
-              <p className="text-sm text-muted-foreground">
-                권구민 지원자에 대한 자세한 이력서 및 포트폴리오를 확인하고 싶다면?
-                
-              </p>
-              <p  className="text-sm text-muted-foreground">
-
-              <a
-                  href="https://fuschia-humor-318.notion.site/baeafb8b2d1f49dea525dab32db51b08"
-                  className="text-primary hover:underline"
-                  target='_blank'
-                  rel="noopener noreferrer"
-                >
-                  여기를 클릭하여 이동하세요!
-                </a>
-              </p>
-                            </div>
-          )}
+  <div className="mx-8 flex h-full flex-col items-center justify-center gap-3 text-center">
+    <Bot size={28} />
+    <p className="text-lg font-medium">
+      권구민 지원자에 대해 뭐든지 물어보세요!
+    </p>
+    <p>
+      그럼, AI 구민이 당신의 질문에 대해 모두 답변 할 것입니다!<br/>
+      만약 어떤 질문을 해야할 지 모르겠다면 <br/> "질문 추천해줘" 라고 물어보세요!
+    </p>
+    <p className="text-sm text-muted-foreground">
+      권구민 지원자에 대한 자세한 이력서 및 포트폴리오를 확인하고 싶다면?{" "}
+      <a
+        href="https://fuschia-humor-318.notion.site/baeafb8b2d1f49dea525dab32db51b08"
+        className="text-primary hover:underline"
+        target='_blank'
+        rel="noopener noreferrer"
+      >
+        여기를 클릭하여 이동하세요!
+      </a>
+    </p>
+  </div>
+)}
         </div>
         <form onSubmit={handleSubmit} className="m-3 flex gap-1">
           <button
