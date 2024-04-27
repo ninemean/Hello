32741f6d07f01abc14e5b0172846fe49faa5771b
ninemean <167946235+ninemean@users.noreply.github.com>
2024-04-27 13:50:41 +0000

    feat: pls

--- my-create-llama-app/app/layout.tsx
+++ my-create-llama-app/app/layout.tsx
@@ -4,9 +4,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import me from "@/assets/higoomin.png";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
+const OG_IMAGE = me || me
+
 export const metadata: Metadata = {
+
+
   // metadataBase: new URL('www.aigoomin.shop'),
   metadataBase: new URL('https://www.aigoomin.shop'),
   title: {
@@ -17,7 +24,16 @@ export const metadata: Metadata = {
   openGraph: {
     url: 'https://www.aigoomin.shop',
   },
+
+ twitter: {
+  title:  "%s | 안녕하세요! 권구민입니다 👋",
+  description: "AI구민에게 권구민 지원자에 대해 뭐든지 질문해보세요!",
+  images: {
+    url: "https://cdn.discordapp.com/attachments/1222405100346150945/1233773021479698432/higoomin.png?ex=662e502a&is=662cfeaa&hm=bd8154d7e030bc4201a5782554c202adc61bfe1668690a811705814b754610ae&",
+  },
+ }
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
